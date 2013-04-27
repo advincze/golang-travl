b64651b4b32fd96103ccacdd508f02d6c2b81f93
adam <advincze@gmail.com>
2013-04-27 10:59:55 +0200

    CRUD interface

--- go/src/travl/travl.go
+++ go/src/travl/travl.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", defaultHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/res", wrapHandler(createHandler)).Methods("PUT")
+	r.HandleFunc("/res/", wrapHandler(createHandler)).Methods("PUT")
+	r.HandleFunc("/res/{id}", wrapHandler(readHandler)).Methods("GET")
+	r.HandleFunc("/res/{id}/", wrapHandler(readHandler)).Methods("GET")
+	r.HandleFunc("/res/{id}", wrapHandler(updateHandler)).Methods("PUT")
+	r.HandleFunc("/res/{id}/", wrapHandler(updateHandler)).Methods("PUT")
+	r.HandleFunc("/res/{id}", wrapHandler(deleteHandler)).Methods("DELETE")
+	r.HandleFunc("/res/{id}/", wrapHandler(deleteHandler)).Methods("DELETE")
+
+	http.Handle("/", r)
+
 	http.ListenAndServe(":8080", nil)
 }
 
-func defaultHandler(w http.ResponseWriter, req *http.Request) {
+func wrapHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Printf("wrap with %v", vars)
+		fn(w, r)
+	}
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "create res\n")
+}
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get(":id")
+	fmt.Fprintf(w, "Hi there, I love %s! \n", id)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "update res\n")
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "delete res\n")
 }
