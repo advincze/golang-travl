e0bd33bf2c78d1ae8e0d1b4bf4383f20985707d2
adam <advincze@gmail.com>
2013-05-03 00:37:02 +0200

    interfaces added, test empty set

--- src/travl/byteav/byteav.go
+++ src/travl/byteav/byteav.go
@@ -1,12 +1,35 @@
 package byteav
 
+import (
+	"time"
+)
+
+type TimeRes int
+
+const Minute TimeRes = 1
+const Minute5 TimeRes = 5 * Minute
+const Minute15 TimeRes = 15 * Minute
+const Hour TimeRes = 60 * Minute
+const Day TimeRes = 24 * Hour
+
 type ByteAv struct {
+	Resolution TimeRes
+	offset     int64
+	arr        []byte
 }
 
-func New() *ByteAv {
-	return &ByteAv{}
+func New(res TimeRes) *ByteAv {
+	return &ByteAv{Resolution: res}
 }
 
-func (b *ByteAv) Len() int {
-	return 0
+func (b *ByteAv) Set(from, to time.Time, value byte) error {
+	return nil
+}
+
+func (b *ByteAv) Get(from, to time.Time) []byte {
+	println("res", int(b.Resolution))
+	println(int(to.Sub(from).Minutes()))
+	length := int(to.Sub(from).Minutes()) / int(b.Resolution)
+	println("length", length)
+	return make([]byte, length)
 }
--- src/travl/byteav/byteav_test.go
+++ src/travl/byteav/byteav_test.go
@@ -1,17 +1,34 @@
 package byteav
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+	"time"
+)
 
-func TestShouldReturnAnEmptyNewByteAv(t *testing.T) {
-	b := New()
-	if b == nil {
-		t.Errorf("Empty set should not be nil")
+var now time.Time = time.Date(1998, time.February, 1, 0, 0, 0, 0, time.UTC)
+
+func TestNewByteAv(t *testing.T) {
+	byteAv := New(Minute5)
+	if byteAv == nil {
+		t.Errorf("Empty byteav should not be nil")
 	}
 }
 
-func TestEmptyNewByteAvShouldHaveLenZero(t *testing.T) {
-	b := New()
-	if b.Len() != 0 {
-		t.Errorf("Empty set should not have length 0")
+func TestSetFromTo(t *testing.T) {
+	byteAv := New(Minute5)
+
+	err := byteAv.Set(now, now.Add(15*time.Minute), 1)
+	if err != nil {
+		t.Errorf("Set should not return an error")
+	}
+}
+
+func TestGetFromEmptyAv(t *testing.T) {
+	byteAv := New(Minute5)
+	byteArr := byteAv.Get(now, now.Add(15*time.Minute))
+	expected := []byte{0, 0, 0}
+	if !bytes.Equal(byteArr, expected) {
+		t.Errorf("empty av should return %v , was %v", expected, byteArr)
 	}
 }
