bc11dd160a41ea9ce230d00480f74e0d6421b03f
adam <advincze@gmail.com>
2013-06-27 23:54:24 +0200

    gob encoding decoding test

--- src/travl/av/db.go
+++ src/travl/av/db.go
@@ -1,12 +1,14 @@
 package av
 
 import (
+	"bytes"
 	"encoding/gob"
 	"github.com/steveyen/gkvlite"
 	"os"
 )
 
 type DB struct {
+	file    *os.File
 	store   *gkvlite.Store
 	objects *gkvlite.Collection
 }
@@ -22,6 +24,7 @@ func NewDB(fname string) *DB {
 	store, _ := gkvlite.NewStore(f)
 	objects := store.SetCollection("objects", nil)
 	return &DB{
+		file:    f,
 		store:   store,
 		objects: objects,
 	}
@@ -32,21 +35,29 @@ func (db *DB) CloseDB() {
 }
 
 func (db *DB) getObject(id string) *object {
-	typeID, err := db.objects.Get([]byte(id))
+	gob.Register((*object)(nil))
+	gobData, err := db.objects.Get([]byte(id))
+	buf := bytes.NewBuffer(gobData)
+	dec := gob.NewDecoder(buf)
 	if err != nil {
-		return nil
+		panic(err)
 	}
-	return &object{ID: id, TypeID: string(typeID)}
+	var obj *object
+	err = dec.Decode(&obj)
+	if err != nil {
+		panic(err)
+	}
+	return obj
 }
 
 func (db *DB) writeObject(obj *object) {
 	gob.Register(obj)
+	m := new(bytes.Buffer)
+	enc := gob.NewEncoder(m)
+	enc.Encode(obj)
 
-	typeID, err := db.objects.Get([]byte(id))
-	if err != nil {
-		return nil
-	}
-	return &object{ID: id, TypeID: string(typeID)}
+	db.objects.Set([]byte(obj.ID), m.Bytes())
+	db.store.Flush()
 }
 
 // fname := "myfile.db"
--- src/travl/av/db_test.go
+++ src/travl/av/db_test.go
@@ -1,6 +1,8 @@
 package av
 
 import (
+	"bytes"
+	"encoding/gob"
 	"os"
 	"testing"
 )
@@ -29,6 +31,54 @@ func TestOpenExistingDB(t *testing.T) {
 func TestWriteObjectIntoDB(t *testing.T) {
 	defer os.Remove(testDBFileName)
 	db := NewDB(testDBFileName)
+
+	obj := &object{ID: "myID", TypeID: "myTypeID"}
+	db.writeObject(obj)
+
 	db.CloseDB()
 
+	db = NewDB(testDBFileName)
+
+	obj2 := db.getObject("myID")
+
+	db.CloseDB()
+
+	if obj.ID != obj2.ID {
+		t.Errorf("the written and retrieved object should be equal, were %s, %s", obj.ID, obj2.ID)
+	}
+
+	if obj.TypeID != obj2.TypeID {
+		t.Errorf("the written and retrieved object should be equal, were %s, %s", obj.TypeID, obj2.TypeID)
+	}
+}
+
+func TestGob(t *testing.T) {
+	type Message struct {
+		ID   string
+		Text string
+	}
+	m := &Message{ID: "myID", Text: "myText"}
+	gob.Register((*Message)(nil))
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(m)
+	if err != nil {
+		panic(err)
+	}
+	bb := buf.Bytes()
+
+	bb2 := make([]byte, len(bb))
+	copy(bb2, bb)
+
+	buf2 := bytes.NewBuffer(bb2)
+	dec := gob.NewDecoder(buf2)
+	if err != nil {
+		panic(err)
+	}
+	var m2 *Message
+	err = dec.Decode(&m2)
+	if err != nil {
+		panic(err)
+	}
+	// println(m2.ID, m2.Text)
 }
